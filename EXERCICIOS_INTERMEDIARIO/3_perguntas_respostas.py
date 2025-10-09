7f52a3966217d2ab4811967a406fc3e76cd252d9
Gabriel <gabrielsconci@gmail.com>
2025-10-09 13:06:14 -0300

    Atualizando a atividade

--- EXERCICIOS_INTERMEDIARIO/3_perguntas_respostas.py
+++ EXERCICIOS_INTERMEDIARIO/3_perguntas_respostas.py
@@ -2,7 +2,7 @@
 # Primeira vez mexendo em dicionario dentro de listas
 # Utilizei o ChatGpt no modo Estudar, para entender como navegar nos dicionarios dentro de uma lista
 
-perguntas = [
+perguntas = [  # Aqui temos o sistema das perguntas
     {'pergunta': 'Quanto é 12+8?',
      'opcoes': ['16', '20', '23', '22'],
      'resposta': '20'
@@ -19,28 +19,29 @@ perguntas = [
 
 while True:
     pontos = 0
-    controle_break = False
-    for pergunta in perguntas:
+    controle_break = False  # Caso True faz o break final ser ativado
+    for pergunta in perguntas:  # Percorre cada dicionario
         print(pergunta['pergunta'])
-        for i, opcao in enumerate(pergunta['opcoes']):
+        for i, opcao in enumerate(pergunta['opcoes']):  # Adicionamos um indice para cada opção
             print(f'[{i}] {opcao}')
         resposta = input('Qual a alternativa correta? ')
-        try:
+        if resposta.isdigit():
             resposta_int = int(resposta)
-        except ValueError:
-            print('Digite um número por favor')
+        else:
+            print('Digite um numero por favor')
+            controle_break = True  # Atualiza para True o controle de break
             break
-        if resposta_int >= 0 and resposta_int <= len(pergunta['opcoes']):
-            if pergunta['opcoes'][resposta_int] == pergunta['resposta']:
+        if resposta_int >= 0 and resposta_int <= len(pergunta['opcoes']):  # Ver se está entre 0 e 3
+            if pergunta['opcoes'][resposta_int] == pergunta['resposta']:  # Navega dentro da lista
                 print('Acertouuu')
                 pontos += 1
             else:
                 print('Errado')
         else:
             print('Digite algo entre 0 e 3')
-            controle_break = True
+            controle_break = True  # Atualiza para True o controle de break
             break
-    if controle_break:
+    if controle_break:  # Caso tenha um erro, quebra o codigo não mostrando os pontos do usuario
         break
     print(f'Você acertou {pontos} de {len(perguntas)}')
     break
